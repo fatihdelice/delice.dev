0ea1b2fd13e0c390dfd657e022e91d2d0e720858
Fatih Delice <delicex58@gmail.com>
2022-11-14 01:13:33 +0300

    buy me coffee added

--- pages/_document.js
+++ pages/_document.js
@@ -41,6 +41,7 @@ export default function Document() {
         </svg>
         <Main />
         <NextScript />
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="fatihdelice" data-description="Support me on Buy me a coffee!" data-message="" data-color="#ecc94b" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </Html>
   );
