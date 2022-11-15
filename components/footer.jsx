a15e297ae61eea2b1c21cd5b91baec08f29b3d3d
Fatih Delice <delicex58@gmail.com>
2022-11-16 00:31:12 +0300

    updated

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,20 +2,11 @@ import Link from "next/link";
 import classNames from "../util/classNames";
 
 const footer = () => {
-  var time = new Date();
-  var fullTime = new Intl.DateTimeFormat("en-EN", {
-    weekday: "short",
-  }).format(time);
-
-  var min =
-    ("0" + time.getHours()).slice(-2) +
-    ":" +
-    ("0" + time.getMinutes()).slice(-2);
 
   return <>
     <div
       className={classNames(
-        "py-4 pl-3 text-xs text-slate-600",
+        "py-4 pl-3 text-xs opacity-40 text-slate-600",
         "sm:pl-12",
         "",
         "dark:text-neutral-300"
@@ -33,9 +24,6 @@ const footer = () => {
         <a href="https://tailwindcss.com/" target="_blank" className="link">
           Vercel
         </a>{" "}
-      </div>
-      <div className="font-bold">
-        Locale: {min} {fullTime}
       </div>
     </div>
   </>;
